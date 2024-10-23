db2af96ec8814146a60ce9c7ea6526bc76f31dcc
maribukh <mariebukhaidze@gmail.com>
2024-10-23 19:23:23 +0400

    Edited task_4 + while

--- task_4/script.js
+++ task_4/script.js
@@ -1,6 +1,17 @@
+// with For
 console.log("Numbers between 100 and 100,000 that are divisible by 123 :");
 for (let i = 100; i <= 100000; i++) {
     if (i % 123 === 0) { 
         console.log(i); 
     }
+}
+
+
+// with While
+console.log("Numbers between 100 and 100,000 that are divisible by 123 ::");
+let i = 100; 
+while (i <= 100000) {
+    if (i % 123 === 0) { 
+        console.log(i); 
+    i++; 
 }
